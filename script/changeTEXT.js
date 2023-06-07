76f9e71f28dc5453791053701bdb8063bb7c8856
Jemagif78 <ermekgd2004@gmail.com>
2023-06-07 23:30:15 +0600

    upd15
    remove some info & media

--- script/changeTEXT.js
+++ script/changeTEXT.js
@@ -5,10 +5,10 @@ function clickTXT01() {
         addEventListener('mouseenter', (event) => {});
         addEventListener('mouseleave', (event) => {});
 
-        document.getElementById("changeTXT01").innerHTML = "+7 708 848 9066";
+        document.getElementById("changeTXT01").innerHTML = "+1 234 567 8900";
         document.getElementById("changeTXT01").style = "color: rgb(170, 200, 255)";
         
-        document.getElementById("changeTXT01").href = "https://wa.me/+77088489066";
+        document.getElementById("changeTXT01").href = "https://wa.me/0";
         document.getElementById("changeTXT01").onclick = "return confirm('[ ? ] do you really want to open this link ?')";
         document.getElementById("changeTXT01").target = "_blank";
     })
@@ -19,10 +19,10 @@ function clickTXT02() {
         addEventListener('mouseenter', (event) => {});
         addEventListener('mouseleave', (event) => {});
 
-        document.getElementById("changeTXT02").innerHTML = "+7 777 073 2414";
+        document.getElementById("changeTXT02").innerHTML = "+1 234 567 8900";
         document.getElementById("changeTXT02").style = "color: rgb(170, 200, 255)";
         
-        document.getElementById("changeTXT02").href = "https://wa.me/+77770732414";
+        document.getElementById("changeTXT02").href = "https://wa.me/0";
         document.getElementById("changeTXT02").onclick = "return confirm('[ ? ] do you really want to open this link ?')";
         document.getElementById("changeTXT02").target = "_blank";
     })
